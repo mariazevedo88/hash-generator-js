f6dfa808f4a84b723e773e7b84a8146cd36d61c2
Ron Green <11993626+georgettica@users.noreply.github.com>
2020-02-09 01:24:05 +0200

    attempt to e2e tests

--- test/e2e/nightwatch.conf.js
+++ test/e2e/nightwatch.conf.js
@@ -1,4 +1,4 @@
-require('babel-register')
+require('@babel/register')
 var config = require('../../config')
 
 // http://nightwatchjs.org/gettingstarted#settings-file
@@ -10,7 +10,7 @@ module.exports = {
   selenium: {
     start_process: true,
     server_path: require('selenium-server').path,
-    host: '127.0.0.1',
+    host: '0.0.0.0',
     port: 4444,
     cli_args: {
       'webdriver.chrome.driver': require('chromedriver').path
@@ -20,10 +20,10 @@ module.exports = {
   test_settings: {
     default: {
       selenium_port: 4444,
-      selenium_host: 'localhost',
+      selenium_host: '0.0.0.0',
       silent: true,
       globals: {
-        devServerURL: 'http://localhost:' + (process.env.PORT || config.dev.port)
+        devServerURL: 'http://0.0.0.0:' + (process.env.PORT || config.dev.port)
       }
     },
 
